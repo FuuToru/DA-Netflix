760f3a4210da514db60c82a40f872230403b3a2a
Tri <huutri231103@gmail.com>
2024-03-30 10:48:05 +0700

    project TKMT

---
+++ netflix.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE NETFLIX
+USE NETFLIX
+
+CREATE TABLE netflix_movies (
+    id INT,
+    type VARCHAR(255),
+    name VARCHAR(255),
+    creator CHAR(255),
+    starring VARCHAR(255),
+    year INT,
+    rating VARCHAR(255),
+    time VARCHAR(255),
+    genres VARCHAR(255),
+    country VARCHAR(255),
+    describle TEXT
+);
+
